686c6448b7c1c66c10bcad750aa5861b3cb85045
jobs.z <jobs.z@51offer.com>
2015-05-08 09:29:21 +0800

    test

--- test/ViewController.m
+++ test/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.view.backgroundColor = [UIColor magentaColor];
+    self.view.backgroundColor = [UIColor orangeColor];
 }
 
 - (void)didReceiveMemoryWarning {
